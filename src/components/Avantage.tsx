fa09d98bd95173a9a302f5e0444d121cd7ec7564
Mohamed <achour_m@etna-alternance.net>
2024-04-23 16:58:20 +0200

    retrecissement font et centrer le text about

--- src/components/Avantage.tsx
+++ src/components/Avantage.tsx
@@ -1,4 +1,4 @@
-import { Container, Box, Typography, CssBaseline } from "@mui/material";
+import { Container, Box, Typography, CssBaseline, useMediaQuery } from "@mui/material";
 
 function AdvantagesSection() {
   const advantages = [
@@ -15,6 +15,9 @@ function AdvantagesSection() {
       description: "",
     },
   ];
+
+  // Utilisation de useMediaQuery pour détecter les écrans de téléphone
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
   return (
     <>
@@ -33,7 +36,7 @@ function AdvantagesSection() {
           variant="h3"
           component="h3"
           fontFamily='"Paytone One", sans-serif !important'
-          sx={{ color: "#0c5245", fontWeight: "bold" }}
+          sx={{ color: "#0c5245", fontWeight: "bold", fontSize: isMobile ? "2rem" : "3rem" }} // Ajustez la taille de la typographie pour les téléphones
           gutterBottom
         >
           Nos avantages
@@ -45,11 +48,12 @@ function AdvantagesSection() {
           marginTop: 4,
           position: 'relative', // Ensure the svg lines are positioned correctly
         }}>
-          <svg width="100%" height="150px" style={{ position: 'absolute', top: '0px', left: '0' }}>
-            <line x1="40%" y1="0" x2="17%" y2="100%" stroke="#0c5245" strokeWidth="5"/>
-            <line x1="50%" y1="0" x2="50%" y2="100%" stroke="#0c5245" strokeWidth="5"/>
-            <line x1="60%" y1="0" x2="82%" y2="100%" stroke="#0c5245" strokeWidth="5"/>
-          </svg>
+         <svg width="100%" style={{ position: 'absolute', top: '0px', left: '0' }}>
+          <line x1="40%" y1="0" x2="17%" y2="100%" stroke="#0c5245" strokeWidth="5"/> {/* Ajustez la longueur de la première ligne */}
+          <line x1="50%" y1="0" x2="50%" y2="200px" stroke="#0c5245" strokeWidth="5"/> {/* Ajustez la longueur et la hauteur de la ligne centrale */}
+          <line x1="60%" y1="0" x2="82%" y2="100%" stroke="#0c5245" strokeWidth="5"/> {/* Ajustez la longueur de la dernière ligne */}
+        </svg>
+       
           {advantages.map((advantage, index) => (
             <Box
               key={index}
@@ -66,16 +70,19 @@ function AdvantagesSection() {
                   fontWeight: 'bold',
                   color: "black",
                   letterSpacing: '0.5px',
-                  marginY: 2
+                  marginY: 2,
+                  fontSize: isMobile ? "1rem" : "2rem", // Ajustez la taille de la typographie pour les téléphones
+                  lineHeight: isMobile ? "1.2" : "1.5", // Ajustez l'espacement des lignes pour les téléphones
                 }}>
                 {advantage.title}
               </Typography>
-              <Typography variant="body1" fontFamily='"Paytone One", sans-serif !important' sx={{ fontWeight: 'bold' }}>
+              <Typography variant="body1" fontFamily='"Paytone One", sans-serif !important' sx={{ fontWeight: 'bold', fontSize: "16px" }}>
                 {advantage.description}
               </Typography>
             </Box>
           ))}
         </Box>
+        
       </Container>
     </>
   );
